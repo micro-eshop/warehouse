a76cbdfdeea768147594e244a844d7d8dd07c6ec
Dominik.Kotecki <dominikus1910@gmail.com>
2022-05-07 07:57:09 +0000

    fix

--- src/Warehouse.Infrastructure/Logging/WarehouseNatsProductCreatedSubscriberLogger.cs
+++ src/Warehouse.Infrastructure/Logging/WarehouseNatsProductCreatedSubscriberLogger.cs
@@ -6,9 +6,9 @@ namespace Warehouse.Infrastructure.Logging;
 
 internal static partial class WarehouseNatsProductCreatedSubscriberLogger
 {
-   [LoggerMessage(EventId = 0, Level = LogLevel.Warning, Message = "Failed to deserialize message: {Message}")]
-   public static partial void LogFailedToDeserializeMessage(this ILogger logger, string message);
+    [LoggerMessage(EventId = 0, Level = LogLevel.Warning, Message = "Failed to deserialize message: {Message}")]
+    public static partial void LogFailedToDeserializeMessage(this ILogger logger, string message);
 
-   [LoggerMessage(EventId = 1, Level = LogLevel.Debug, Message = "Product created: {Product}")]
+    [LoggerMessage(EventId = 1, Level = LogLevel.Debug, Message = "Product created: {Product}")]
     public static partial void LogProductsCreated(this ILogger logger, ProductCreated product);
 }
