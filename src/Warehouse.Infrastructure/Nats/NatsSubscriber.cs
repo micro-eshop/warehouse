d87fae62aa453cc47d941d132e94ec40345e6fd0
Dominik.Kotecki <dominikus1910@gmail.com>
2022-07-08 19:27:57 +0200

    add nats open telemetry

--- src/Warehouse.Infrastructure/Nats/NatsSubscriber.cs
+++ src/Warehouse.Infrastructure/Nats/NatsSubscriber.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -30,28 +31,30 @@ internal partial class ProductCreatedJsonContext : JsonSerializerContext
 {
 }
 
+internal record NatsMessage(ProductCreated Msg, ActivityContext? Context);
+
 internal class WarehouseNatsProductCreatedSubscriber : BackgroundService
 {
     private readonly IConnection _connection;
     private readonly ILogger<WarehouseNatsProductCreatedSubscriber> _logger;
     private IAsyncSubscription? _subscription;
     private readonly ISender _sender;
-
-    private readonly Channel<ProductCreated> _channel;
+    public const string Subject = "PRODUCTS.created";
+    private readonly Channel<NatsMessage> _channel;
 
     public WarehouseNatsProductCreatedSubscriber(IConnection connection, ILogger<WarehouseNatsProductCreatedSubscriber> logger, ISender sender)
     {
         _connection = connection;
         _logger = logger;
         _sender = sender;
-        _channel = Channel.CreateBounded<ProductCreated>(new BoundedChannelOptions(10) { SingleReader = true, SingleWriter = true, FullMode = BoundedChannelFullMode.Wait });
+        _channel = Channel.CreateBounded<NatsMessage>(new BoundedChannelOptions(10) { SingleReader = true, SingleWriter = true, FullMode = BoundedChannelFullMode.Wait });
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation("Start listening for product created events");
         var subscribe = Subscribe(_channel.Writer, _logger);
-        _subscription = _connection.SubscribeAsync("PRODUCTS.created");
+        _subscription = _connection.SubscribeAsync(Subject);
         _subscription.MessageHandler += subscribe;
         var processor = ProcessMessages(_channel.Reader, _sender, _logger, stoppingToken);
         _subscription.Start();
@@ -60,27 +63,45 @@ internal class WarehouseNatsProductCreatedSubscriber : BackgroundService
     }
 
 
-    private static EventHandler<MsgHandlerEventArgs> Subscribe(ChannelWriter<ProductCreated> writer, ILogger logger)
+    private static EventHandler<MsgHandlerEventArgs> Subscribe(ChannelWriter<NatsMessage> writer, ILogger logger)
     {
-        return (sender, args) =>
+        return (_, args) =>
         {
+            using var activity = NatsOpenTelemetry.NatsSource.StartActivity(name: "nats.consumer",
+                ActivityKind.Consumer,
+                NatsOpenTelemetry.GetHeaderFromProps(args.Message.Header).ActivityContext);
+            if (activity is not null)
+            {
+                activity.SetTag("messaging.destination", args.Message.Subject);
+                activity.SetTag("messaging.system", "nats");
+                activity.SetTag("messaging.destination_kind", "subject");
+                activity.SetTag("messaging.protocol", "nats");
+            }
             var body = args.Message.Data;
             var json = Encoding.UTF8.GetString(body);
             var product = JsonSerializer.Deserialize(json, ProductCreatedJsonContext.Default.ProductCreated);
             if (product is null)
             {
                 logger.LogFailedToDeserializeMessage(json);
+                activity?.AddEvent(new ActivityEvent("message is null or cannot be deserialized"));
                 return;
             }
-            writer.TryWrite(product);
+            writer.TryWrite(new NatsMessage(product, activity?.Context));
         };
     }
-    private static async Task ProcessMessages(ChannelReader<ProductCreated> reader, ISender sender, ILogger logger, CancellationToken cancellationToken)
+    private static async Task ProcessMessages(ChannelReader<NatsMessage> reader, ISender sender, ILogger logger, CancellationToken cancellationToken)
     {
         await foreach (var msg in reader.ReadAllAsync(cancellationToken).WithCancellation(cancellationToken))
         {
-            logger.LogProductsCreated(msg);
-            await sender.Send(new CreateWarehouseStateCommand(new Core.Model.ProductId(msg.Id)), cancellationToken);
+            var links = msg.Context.HasValue ? new[] { new ActivityLink(msg.Context.Value) } : null;
+            using var activity = NatsOpenTelemetry.NatsSource.StartActivity(ActivityKind.Internal,
+                name: "message.consumption", links: links);
+            if (activity is not null)
+            {
+                activity.SetTag("product.id", msg.Msg.Id);
+            }
+            logger.LogProductsCreated(msg.Msg);
+            await sender.Send(new CreateWarehouseStateCommand(new Core.Model.ProductId(msg.Msg.Id)), cancellationToken);
         }
     }
 
